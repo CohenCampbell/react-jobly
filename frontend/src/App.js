fa9923a541d600c09cd9134117ea23e8cdcb92d2
someguy <someguy@TheSuperUltraOmegaPhoneWavePremium>
2024-03-07 00:46:20 -0500

    Finished routes, and front end logic, need to style

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,35 +1,91 @@
-import React from 'react';
-import {Route, Routes, Link} from 'react-router-dom'
-import Home from './Home';
-import CompanyList from './ComanyList';
-import CompanyDetails from './CompanyDetails';
-import JobList from './JobList';
-import Login from './Login';
-import Signup from './Signup';
-import Profile from './Profile';
-
+import React, { useEffect } from "react";
+import {
+  Route,
+  Routes,
+  Link,
+  useNavigate,
+  useLocation,
+} from "react-router-dom";
+import Home from "./Home";
+import CompanyList from "./ComanyList";
+import CompanyDetails from "./CompanyDetails";
+import JobList from "./JobList";
+import Login from "./Login";
+import Signup from "./Signup";
+import Profile from "./Profile";
+import Welcome from "./Welcome";
 
 function App() {
-  return (
-    <div className="App">
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  useEffect(() => {
+    const token = sessionStorage.getItem("token");
+    const publicPaths = ["/login", "/signup", "/welcome"];
+
+    if (!token && !publicPaths.includes(location.pathname))
+      navigate("/welcome");
+  }, [navigate, location.pathname]);
+
+  function renderNavHtml() {
+    const token = sessionStorage.getItem("token");
+    if (!token) {
+      return (
+        <nav>
+          <div>
+            <Link to="/welcome" id="HomeNavLink">
+              Jobly
+            </Link>
+            <Link to="/signup">
+              <span>Signup</span>
+            </Link>
+            <Link to="/login">
+              <span>Login</span>
+            </Link>
+          </div>
+        </nav>
+      );
+    }
+    return (
       <nav>
         <div>
-        <Link to="/" id='HomeNavLink'>Jobly</Link>
-        <Link to="/companies"><span>Companies</span></Link>
-        <Link to="/jobs"><span>Jobs</span></Link>
-        <Link to="/profile"><span>Profile</span></Link>
-        <Link to="/logout"><span>Logout</span></Link>
+          <Link to="/" id="HomeNavLink">
+            Jobly
+          </Link>
+          <Link to="/companies">
+            <span>Companies</span>
+          </Link>
+          <Link to="/jobs">
+            <span>Jobs</span>
+          </Link>
+          <Link to="/profile">
+            <span>Profile</span>
+          </Link>
+          <Link to="/welcome">
+            <span onClick={handleLogout}>Logout</span>
+          </Link>
         </div>
       </nav>
-    <Routes>
-      <Route path='/' element={<Home/>}/>
-      <Route path='/companies' element={<CompanyList/>}/>
-      <Route path='/companies:name' element={<CompanyDetails/>}/>
-      <Route path='/jobs' element={<JobList/>}/>
-      <Route path='/login' element={<Login/>}/>
-      <Route path='/signup' element={<Signup/>}/>
-      <Route path='/profile' element={<Profile/>}/>
-    </Routes>
+    );
+  }
+
+  function handleLogout() {
+    sessionStorage.removeItem("token");
+  }
+
+  return (
+    <div className="App">
+      {renderNavHtml()}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="welcome" element={<Welcome />} />
+        <Route path="/companies" element={<CompanyList />} />
+        <Route path="/companies/:handle" element={<CompanyDetails />} />
+        <Route path="/jobs" element={<JobList />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
     </div>
   );
 }
--- frontend/src/JoblyApi.js
+++ frontend/src/JoblyApi.js
@@ -21,9 +21,7 @@ class JoblyApi {
     //this has been provided to show you another way to pass the token. you are only expected to read this code for this project.
     const url = `${BASE_URL}/${endpoint}`;
     const headers = { Authorization: `Bearer ${JoblyApi.token}` };
-    const params = (method === "get")
-        ? data
-        : {};
+    const params = method === "get" ? data : {};
 
     try {
       return (await axios({ url, method, data, params, headers })).data;
@@ -34,19 +32,62 @@ class JoblyApi {
     }
   }
 
-  // Individual API routes
+  // get all companies
+  static async getCompanies() {
+    let res = await this.request("companies");
+    return res.companies;
+  }
 
   /** Get details on a company by handle. */
-
   static async getCompany(handle) {
     let res = await this.request(`companies/${handle}`);
     return res.company;
   }
 
-  // obviously, you'll add a lot here ...
+  static async getJobs() {
+    let res = await this.request("jobs");
+    return res.jobs;
+  }
+
+  static async getJob(handle) {
+    let res = await this.request(`jobs/${handle}`);
+    return res.job;
+  }
+
+  static async register(newUserInfo) {
+    let res = await this.request("auth/register", newUserInfo, "post");
+    return res.token;
+  }
+
+  static async login(userInfo) {
+    let res = await this.request("auth/token", userInfo, "post");
+    return res.token;
+  }
+
+  static async getUser(username) {
+    let res = await this.request(`users/${username}`);
+    return res.user;
+  }
+
+  static async updateUser(userInfo) {
+    let res = await this.request(
+      `users/${sessionStorage.getItem("username")}`,
+      userInfo,
+      "patch"
+    );
+    return res.user;
+  }
+
+  static async applyJob(jobId) {
+    let res = await this.request(
+      `users/${sessionStorage.getItem("username")}/jobs/${jobId}`,
+      {},
+      "post"
+    );
+    return res.applied;
+  }
 }
 
-// for now, put token ("testuser" / "password" on class)
-JoblyApi.token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZ" +
-    "SI6InRlc3R1c2VyIiwiaXNBZG1pbiI6ZmFsc2UsImlhdCI6MTU5ODE1OTI1OX0." +
-    "FtrMwBQwe6Ue-glIFgz_Nf8XxRT2YecFCiSpYL0fCXc";
+JoblyApi.token = sessionStorage.getItem("token");
+
+export default JoblyApi;
